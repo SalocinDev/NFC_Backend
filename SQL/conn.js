972674ade03e9513b40ffb6b00d170c34e227e92
Phalluis <nicholaslonganilla@gmail.com>
2025-08-17 19:10:20 +0800

    con

--- SQL/conn.js
+++ SQL/conn.js
@@ -1,9 +1,9 @@
 const mysql2 = require('mysql2/promise');
 
 const pool = mysql2.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
+  host: '172.26.13.248',
+  user: 'Capstone',
+  password: '12345',
   database: 'logintest',
   waitForConnections: true,
   connectionLimit: 10,
