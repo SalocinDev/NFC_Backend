98295612fb90effe557529e2d0a4c07ce370ce3c
Phalluis <nicholaslonganilla@gmail.com>
2025-08-21 15:50:09 +0800

    t

--- Crypto/otp_test.js
+++ Crypto/otp_test.js
@@ -3,7 +3,7 @@ const crypto = require('crypto')
 const generateOTP = () => new Promise(res =>
 	crypto.randomBytes(3, (err, buffer) => {
 		res(
-			parseInt(buffer.toString("hex"), 12)
+			parseInt(buffer.toString("hex"), 16)
 				.toString()
 		);
 	})
