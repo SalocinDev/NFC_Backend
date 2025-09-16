4912b02d1b1b39ec799374326b27dbf814169db6
Nicholas <nicholaslonganilla@gmail.com>
2025-09-16 18:30:24 +0800

    book crud prepare statement

--- Routes/mysql-orvie.js
+++ Routes/mysql-orvie.js
@@ -5,7 +5,7 @@ const pool = require('../SQL/conn');
 //pre sa routes dapat ito VVV
 //ang mga nasa SQL folder ay sql functions lang
 //nich sa list ng book to sa book table
-router.post('/', async (req, res) => {
+router.get('/', async (req, res) => {
     try{
         const [rows] = await pool.query("SELECT * FROM book_table");
         res.status(200).json({ success: true, data: rows[0] });
@@ -35,38 +35,53 @@ router.post("/:bookid", async (req, res) => {
 router.post('/', async (req, res) => {
     const { book_title_fe, book_img_fe, book_author_fe, book_description_fe, book_publisher_fe, book_year_publish_fe, book_status_fe, book_inventory_fe} = res.body;
     if(!book_title_fe || !book_img_fe || !book_author_fe || !book_description_fe || !book_publisher_fe || !book_year_publish_fe || !book_status_fe || !book_inventory_fe) return res.status(400).json({error: "put all books information"});
-    
+
     try{
         const [rows] = await pool.query("INSERT INTO books_table (book_title, book_img, book_author, book_author, book_description, book_publisher, book_year_publish, book_status, book_inventory) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        res.status(200).json({ success: true, data: rows });
+        res.status(201).json({ book_id: result.insertedId, book_title_fe });
     }
     catch(err){ 
         console.error(err);
-        res.status(500).json({ success: false, error:"FAILLLEDDD" });
+        res.status(500).json({error:"FAILLLEDDD" });
   }
 });
 
 //update
-router.put('/', async (req, res) => {
+router.put('/:bookid', async (req, res) => {
     const { book_title_fe, book_img_fe, book_author_fe, book_description_fe, book_publisher_fe, book_year_publish_fe, book_status_fe, book_inventory_fe} = res.body;
-    if(!book_title_fe || !book_img_fe || !book_author_fe || !book_description_fe || !book_publisher_fe || !book_year_publish_fe || !book_status_fe || !book_inventory_fe) return res.status(400).json({error: "put all books information"});
+    const { bookid } = req.params;
+    if(!book_title_fe || !book_img_fe || !book_author_fe || !book_description_fe || !book_publisher_fe || !book_year_publish_fe || !book_status_fe || !book_inventory_fe) 
+        return res.status(400).json({error: "put all books information"});
     
     try{
-        const [rows] = await pool.query("INSERT INTO books_table (book_title, book_img, book_author, book_author, book_description, book_publisher, book_year_publish, book_status, book_inventory) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        res.status(200).json({ success: true, data: rows });
-    }
-    catch(err){ 
+        const [result] = await pool.query(
+            "UPDATE book_table SET book_title =?, book_img = ? , book_author = ? , book_author = ? , book_description = ? , book_publisher = ? , book_year_publish = ? , book_status = ? , book_inventory = ?" , 
+            [book_title_fe, book_img_fe, book_author_fe, book_description_fe, book_publisher_fe, book_year_publish_fe, book_status_fe, book_inventory_fe]
+        );  
+
+        if (result.affectedRows === 0 ) return res.status(404).json({error:  "Book not Found"});
+        res.json(500).json({ bookid, book_title_fe});
+    } catch (err){ 
         console.error(err);
-        res.status(500).json({ success: false, error:"FAILLLEDDD" });
+        res.status(500).json({error:"FAILLLEDDD to update" });
   }
 });
 
 //delete
+router.delete("/:bookid", async (req, res) => {
+  const { id } = req.params;
 
+  try {
+    const [result] = await pool.query("DELETE FROM book_table WHERE book_id = ?", [bookid]);
 
+    if (result.affectedRows === 0) return res.status(404).json({ error: "Book not found" });
 
-/* router.delete(); */
-
+    res.json({ message: "Book deleted successfully" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to delete book" });
+  }
+});
 
 
 //SERVICE
